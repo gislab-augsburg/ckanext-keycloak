57f3243ca3c658c116e445cb2b1c04e70bb514f6
gislab-augsburg <57507078+gislab-augsburg@users.noreply.github.com>
2024-10-14 15:10:10 +0200

    Update plugin.py, add ITranslation

--- ckanext/keycloak/plugin.py
+++ ckanext/keycloak/plugin.py
@@ -1,14 +1,19 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 from ckanext.keycloak.views import get_blueprint
 from ckanext.keycloak import helpers as h
 
 
-class KeycloakPlugin(plugins.SingletonPlugin):
+class KeycloakPlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.ITranslation, inherit=True)
+
+    def i18n_domain(self):
+        return 'ckanext-keycloak'
 
     # IConfigurer
 
