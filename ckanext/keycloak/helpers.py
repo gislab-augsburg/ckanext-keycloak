1f57fcbeed1836a9f012a689176957c93892c017
Martin Bayr <ex.bayr@muenchen.de>
2026-02-10 11:44:42 +0100

    Change autogenerated password length for user creation to 16

--- ckanext/keycloak/helpers.py
+++ ckanext/keycloak/helpers.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 def generate_password():
     alphabet = string.ascii_letters + string.digits
-    return ''.join(secrets.choice(alphabet) for _ in range(8))
+    return ''.join(secrets.choice(alphabet) for _ in range(16))
 
 
 def ensure_unique_username_from_email(email):
